26d8621c87f2f00970e374afbc649720aae701e9
sgangj200 <srinivasrao_gangji@comcast.com>
2017-12-27 10:19:39 -0700

    test commit

--- courses/developingapps/nodejs/datastore/start/server/app.js
+++ courses/developingapps/nodejs/datastore/start/server/app.js
@@ -11,54 +11,54 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-'use strict';
+"use strict"
 
-const path = require('path');
-const express = require('express');
-const config = require('./config');
+const path = require("path")
+const express = require("express")
+const config = require("./config")
 
-const app = express();
+const app = express()
 
-// Static files
-app.use(express.static('server/public/'));
+// Static files //test
+app.use(express.static("server/public/"))
 
-app.disable('etag');
-app.set('views', path.join(__dirname, 'web-app/views'));
-app.set('view engine', 'pug');
-app.set('trust proxy', true);
-app.set('json spaces', 2);
+app.disable("etag")
+app.set("views", path.join(__dirname, "web-app/views"))
+app.set("view engine", "pug")
+app.set("trust proxy", true)
+app.set("json spaces", 2)
 
 // Questions
-app.use('/questions', require('./web-app/questions'));
+app.use("/questions", require("./web-app/questions"))
 
 // Quizzes API
-app.use('/api/quizzes', require('./api'));
+app.use("/api/quizzes", require("./api"))
 
 // Display the home page
-app.get('/', (req, res) => {
-  res.render('home.pug');
-});
+app.get("/", (req, res) => {
+  res.render("home.pug")
+})
 
 // Basic 404 handler
 app.use((req, res) => {
-  res.status(404).send('Not Found');
-});
+  res.status(404).send("Not Found")
+})
 
 // Basic error handler
 app.use((err, req, res, next) => {
   /* jshint unused:false */
-  console.error(err);
+  console.error(err)
   // If our routes specified a specific response, then send that. Otherwise,
   // send a generic message so as not to leak anything.
-  res.status(500).send(err.response || 'Something broke!');
-});
+  res.status(500).send(err.response || "Something broke!")
+})
 
 if (module === require.main) {
   // Start the server
-  const server = app.listen(config.get('PORT'), () => {
-    const port = server.address().port;
-    console.log(`App listening on port ${port}`);
-  });
+  const server = app.listen(config.get("PORT"), () => {
+    const port = server.address().port
+    console.log(`App listening on port ${port}`)
+  })
 }
 
-module.exports = app;
+module.exports = app
